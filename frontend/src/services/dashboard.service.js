bcdef48b57824f2dc941f98fe4bd8893ace84620
unknown <deepakpatel240204@gmail.com>
2026-07-09 17:21:10 +0530

    frontend bug fixed

--- frontend/src/services/dashboard.service.js
+++ frontend/src/services/dashboard.service.js
@@ -4,5 +4,10 @@ export const dashboardService = {
   getSummary: async () => {
     const response = await api.get('/dashboard/summary');
     return response.data;
+  },
+
+  getDevicesStatus: async () => {
+    const response = await api.get('/dashboard/devices');
+    return response.data;
   }
 };
